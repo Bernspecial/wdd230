e239e2858241bd930f7fbd86791c7f04642b66ed
Bernspecial <michaelbernardolayemi@gmail.com>
2024-06-10 15:41:19 +0100

    Course Hmoepage

---
+++ scripts/getDates.js
@@ -0,0 +1,10 @@
+const dateOption = {
+    year: "numeric"
+};
+
+document.querySelector("#year").textContent = new Date().toLocaleDateString("en-Us", dateOption);
+
+const lastModified = document.lastModified;
+color = document.querySelector("#lastModified");
+color.style.color = "#FFCCCC";
+color.textContent = (`This site is last modified: ${lastModified}`);
